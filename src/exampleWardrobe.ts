042e099262ccbd587cf512d5959909ab60f42210
lanewells <180503664+lanewells@users.noreply.github.com>
2026-01-29 21:03:36 -0800

    feat: expanded example wardrobe data

--- src/exampleWardrobe.ts
+++ src/exampleWardrobe.ts
@@ -8,6 +8,7 @@ export const exampleWardrobe: WardrobeItem[] = [
     drawer: "Tops",
     color: "black",
     price_usd: 25,
+    image_url: "./assets/image.jpg",
     favorite: true
   },
   {
@@ -17,6 +18,884 @@ export const exampleWardrobe: WardrobeItem[] = [
     drawer: "Bottoms",
     color: "navy",
     price_usd: 89,
+    image_url: "./assets/image.jpg",
+    favorite: false
+  },
+  {
+    id: "3",
+    name: "Analog watch",
+    brand: "Timex",
+    drawer: "Accessories",
+    color: "silver",
+    price_usd: 65,
+    image_url: "./assets/image.jpg",
+    favorite: false
+  },
+
+  {
+    id: "4",
+    name: "Orange drawstring Bermuda shorts",
+    brand: "Paloma Wool",
+    drawer: "Bottoms",
+    color: "orange",
+    price_usd: 110,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "5",
+    name: "Light wash denim shorts",
+    brand: "Agolde",
+    drawer: "Bottoms",
+    color: "blue",
+    price_usd: 128,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "6",
+    name: "Blue running shorts",
+    brand: "Outdoor Voices",
+    drawer: "Bottoms",
+    color: "blue",
+    price_usd: 58,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "7",
+    name: "Black spandex bike shorts",
+    brand: "Girlfriend Collective",
+    drawer: "Bottoms",
+    color: "black",
+    price_usd: 68,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "8",
+    name: "Low-rise blue shorts",
+    brand: "I.AM.GIA",
+    drawer: "Bottoms",
+    color: "blue",
+    price_usd: 95,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "9",
+    name: "High-waisted white shorts",
+    brand: "Everlane",
+    drawer: "Bottoms",
+    color: "white",
+    price_usd: 78,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+
+  {
+    id: "10",
+    name: "Classic blue jeans",
+    brand: "Levi's",
+    drawer: "Bottoms",
+    color: "blue",
+    price_usd: 98,
+    image_url: "/placeholder-img.jpg",
+    favorite: true
+  },
+  {
+    id: "11",
+    name: "Khaki chinos",
+    brand: "Alex Mill",
+    drawer: "Bottoms",
+    color: "khaki",
+    price_usd: 145,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "12",
+    name: "Olive cargo pants",
+    brand: "Carhartt WIP",
+    drawer: "Bottoms",
+    color: "olive",
+    price_usd: 135,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "13",
+    name: "Grey joggers",
+    brand: "Alo Yoga",
+    drawer: "Bottoms",
+    color: "grey",
+    price_usd: 118,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "14",
+    name: "Grey leggings",
+    brand: "Lululemon",
+    drawer: "Bottoms",
+    color: "grey",
+    price_usd: 98,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+
+  {
+    id: "15",
+    name: "Pink mini skirt",
+    brand: "Motel Rocks",
+    drawer: "Bottoms",
+    color: "pink",
+    price_usd: 55,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "16",
+    name: "Black tennis skirt",
+    brand: "Nike",
+    drawer: "Bottoms",
+    color: "black",
+    price_usd: 65,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "17",
+    name: "Brown maxi skirt",
+    brand: "Reformation",
+    drawer: "Bottoms",
+    color: "brown",
+    price_usd: 168,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "18",
+    name: "Blue A-line skirt",
+    brand: "Sézane",
+    drawer: "Bottoms",
+    color: "blue",
+    price_usd: 135,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "19",
+    name: "Yellow skort",
+    brand: "Lazy Oaf",
+    drawer: "Bottoms",
+    color: "yellow",
+    price_usd: 85,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+
+  {
+    id: "20",
+    name: "White graphic tee",
+    brand: "Pleasures",
+    drawer: "Tops",
+    color: "white",
+    price_usd: 55,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "21",
+    name: "Red v-neck tee",
+    brand: "Everlane",
+    drawer: "Tops",
+    color: "red",
+    price_usd: 38,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "22",
+    name: "Teal crewneck tee",
+    brand: "Uniqlo U",
+    drawer: "Tops",
+    color: "teal",
+    price_usd: 25,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "23",
+    name: "Blue long sleeve tee",
+    brand: "Arket",
+    drawer: "Tops",
+    color: "blue",
+    price_usd: 45,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+
+  {
+    id: "24",
+    name: "White button-down blouse",
+    brand: "Totême",
+    drawer: "Tops",
+    color: "white",
+    price_usd: 220,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "25",
+    name: "Purple peplum blouse",
+    brand: "For Love & Lemons",
+    drawer: "Tops",
+    color: "purple",
+    price_usd: 165,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "26",
+    name: "Mauve wrap blouse",
+    brand: "Reformation",
+    drawer: "Tops",
+    color: "mauve",
+    price_usd: 148,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "27",
+    name: "Pink sleeveless blouse",
+    brand: "& Other Stories",
+    drawer: "Tops",
+    color: "pink",
+    price_usd: 79,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+
+  {
+    id: "28",
+    name: "Red halter tank",
+    brand: "Miaou",
+    drawer: "Tops",
+    color: "red",
+    price_usd: 95,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "29",
+    name: "Black racerback tank",
+    brand: "Skims",
+    drawer: "Tops",
+    color: "black",
+    price_usd: 38,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "30",
+    name: "Taupe muscle tank",
+    brand: "Leset",
+    drawer: "Tops",
+    color: "taupe",
+    price_usd: 88,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "31",
+    name: "Black spaghetti strap tank",
+    brand: "Aritzia",
+    drawer: "Tops",
+    color: "black",
+    price_usd: 45,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "32",
+    name: "Rainbow cardigan sweater",
+    brand: "Lazy Oaf",
+    drawer: "Tops",
+    color: "multicolor",
+    price_usd: 165,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "33",
+    name: "Teal pullover sweater",
+    brand: "Everlane",
+    drawer: "Tops",
+    color: "teal",
+    price_usd: 98,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "34",
+    name: "Cream turtleneck sweater",
+    brand: "COS",
+    drawer: "Tops",
+    color: "cream",
+    price_usd: 125,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "35",
+    name: "Grey crewneck sweater",
+    brand: "Arket",
+    drawer: "Tops",
+    color: "grey",
+    price_usd: 110,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+
+  {
+    id: "36",
+    name: "Burgundy shift dress",
+    brand: "& Other Stories",
+    drawer: "One-pieces",
+    color: "burgundy",
+    price_usd: 129,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "37",
+    name: "Little black dress",
+    brand: "Reformation",
+    drawer: "One-pieces",
+    color: "black",
+    price_usd: 198,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "38",
+    name: "White slip dress",
+    brand: "Anine Bing",
+    drawer: "One-pieces",
+    color: "white",
+    price_usd: 250,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "39",
+    name: "Turquoise bodycon dress",
+    brand: "Poster Girl",
+    drawer: "One-pieces",
+    color: "turquoise",
+    price_usd: 210,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "40",
+    name: "Blush ruffle dress",
+    brand: "For Love & Lemons",
+    drawer: "One-pieces",
+    color: "blush",
+    price_usd: 235,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+
+  {
+    id: "41",
+    name: "Navy romper jumpsuit",
+    brand: "Madewell",
+    drawer: "One-pieces",
+    color: "navy",
+    price_usd: 118,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "42",
+    name: "Black wide-leg jumpsuit",
+    brand: "Norma Kamali",
+    drawer: "One-pieces",
+    color: "black",
+    price_usd: 165,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "43",
+    name: "Marine professional jumpsuit",
+    brand: "MM.LaFleur",
+    drawer: "One-pieces",
+    color: "marine",
+    price_usd: 285,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "44",
+    name: "Forest green utility jumpsuit",
+    brand: "Boiler Suit Co.",
+    drawer: "One-pieces",
+    color: "forest green",
+    price_usd: 190,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+
+  {
+    id: "45",
+    name: "Denim overalls",
+    brand: "Levi's",
+    drawer: "One-pieces",
+    color: "blue",
+    price_usd: 128,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "46",
+    name: "Camel corduroy overalls",
+    brand: "BDG",
+    drawer: "One-pieces",
+    color: "camel",
+    price_usd: 110,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "47",
+    name: "Black shortalls",
+    brand: "Lucy & Yak",
+    drawer: "One-pieces",
+    color: "black",
+    price_usd: 95,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+
+  {
+    id: "48",
+    name: "White low-top sneakers",
+    brand: "Veja",
+    drawer: "Shoes",
+    color: "white",
+    price_usd: 150,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "49",
+    name: "White high-top sneakers",
+    brand: "Converse",
+    drawer: "Shoes",
+    color: "white",
+    price_usd: 85,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "50",
+    name: "Red slip-on sneakers",
+    brand: "Vans",
+    drawer: "Shoes",
+    color: "red",
+    price_usd: 60,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "51",
+    name: "Grey running sneakers",
+    brand: "New Balance",
+    drawer: "Shoes",
+    color: "grey",
+    price_usd: 140,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "52",
+    name: "Brown cowboy boots",
+    brand: "Tecovas",
+    drawer: "Shoes",
+    color: "brown",
+    price_usd: 295,
+    image_url: "/placeholder-img.jpg",
+    favorite: true
+  },
+  {
+    id: "53",
+    name: "Black knee-high boots",
+    brand: "Vagabond",
+    drawer: "Shoes",
+    color: "black",
+    price_usd: 220,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "54",
+    name: "Tan steel-toe boots",
+    brand: "Blundstone",
+    drawer: "Shoes",
+    color: "tan",
+    price_usd: 210,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "55",
+    name: "Blue snow boots",
+    brand: "Sorel",
+    drawer: "Shoes",
+    color: "blue",
+    price_usd: 180,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+
+  {
+    id: "56",
+    name: "Grey oxfords",
+    brand: "Dr. Martens",
+    drawer: "Shoes",
+    color: "grey",
+    price_usd: 140,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "57",
+    name: "Blush heels",
+    brand: "Stuart Weitzman",
+    drawer: "Shoes",
+    color: "blush",
+    price_usd: 295,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "58",
+    name: "Hot pink platform heels",
+    brand: "Jeffrey Campbell",
+    drawer: "Shoes",
+    color: "hot pink",
+    price_usd: 150,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "59",
+    name: "Red Crocs",
+    brand: "Crocs",
+    drawer: "Shoes",
+    color: "red",
+    price_usd: 50,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+
+  {
+    id: "60",
+    name: "Tan Birkenstock sandals",
+    brand: "Birkenstock",
+    drawer: "Shoes",
+    color: "tan",
+    price_usd: 110,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "61",
+    name: "Nude espadrilles",
+    brand: "Castañer",
+    drawer: "Shoes",
+    color: "nude",
+    price_usd: 145,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "62",
+    name: "Yellow flip-flops",
+    brand: "Havaianas",
+    drawer: "Shoes",
+    color: "yellow",
+    price_usd: 28,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "63",
+    name: "Charcoal slides",
+    brand: "Adidas",
+    drawer: "Shoes",
+    color: "charcoal",
+    price_usd: 45,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "64",
+    name: "Green baseball cap",
+    brand: "Sporty & Rich",
+    drawer: "Accessories",
+    color: "green",
+    price_usd: 65,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "65",
+    name: "Yellow bucket hat",
+    brand: "Baggu",
+    drawer: "Accessories",
+    color: "yellow",
+    price_usd: 48,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "66",
+    name: "Red beanie",
+    brand: "Acne Studios",
+    drawer: "Accessories",
+    color: "red",
+    price_usd: 120,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "67",
+    name: "Blue visor",
+    brand: "Nike",
+    drawer: "Accessories",
+    color: "blue",
+    price_usd: 30,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+
+  {
+    id: "68",
+    name: "Black leather belt",
+    brand: "Madewell",
+    drawer: "Accessories",
+    color: "black",
+    price_usd: 52,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "69",
+    name: "Off-white canvas belt",
+    brand: "Carhartt WIP",
+    drawer: "Accessories",
+    color: "off-white",
+    price_usd: 45,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "70",
+    name: "Tan braided belt",
+    brand: "Anderson’s",
+    drawer: "Accessories",
+    color: "tan",
+    price_usd: 135,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "71",
+    name: "Brown buckled belt",
+    brand: "A.P.C.",
+    drawer: "Accessories",
+    color: "brown",
+    price_usd: 110,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+
+  {
+    id: "72",
+    name: "Nude trench coat",
+    brand: "Totême",
+    drawer: "Outerwear",
+    color: "nude",
+    price_usd: 690,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "73",
+    name: "Black puffer coat",
+    brand: "The North Face",
+    drawer: "Outerwear",
+    color: "black",
+    price_usd: 320,
+    image_url: "/placeholder-img.jpg",
+    favorite: true
+  },
+  {
+    id: "74",
+    name: "Sage green snow coat",
+    brand: "Patagonia",
+    drawer: "Outerwear",
+    color: "sage green",
+    price_usd: 399,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "75",
+    name: "Red parka",
+    brand: "Canada Goose",
+    drawer: "Outerwear",
+    color: "red",
+    price_usd: 895,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+
+  {
+    id: "76",
+    name: "Brown bomber jacket",
+    brand: "Alpha Industries",
+    drawer: "Outerwear",
+    color: "brown",
+    price_usd: 190,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "77",
+    name: "Brown leather jacket",
+    brand: "AllSaints",
+    drawer: "Outerwear",
+    color: "brown",
+    price_usd: 499,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "78",
+    name: "Navy blazer",
+    brand: "Theory",
+    drawer: "Outerwear",
+    color: "navy",
+    price_usd: 395,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "79",
+    name: "Blue denim jacket",
+    brand: "Levi’s",
+    drawer: "Outerwear",
+    color: "blue",
+    price_usd: 118,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "80",
+    name: "Purple windbreaker",
+    brand: "Columbia",
+    drawer: "Outerwear",
+    color: "purple",
+    price_usd: 95,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+
+  {
+    id: "81",
+    name: "Black leather gloves",
+    brand: "Hestra",
+    drawer: "Accessories",
+    color: "black",
+    price_usd: 140,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "82",
+    name: "Grey wool gloves",
+    brand: "Uniqlo",
+    drawer: "Accessories",
+    color: "grey",
+    price_usd: 25,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "83",
+    name: "White snow gloves",
+    brand: "Burton",
+    drawer: "Accessories",
+    color: "white",
+    price_usd: 85,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "84",
+    name: "Black work gloves",
+    brand: "Carhartt",
+    drawer: "Accessories",
+    color: "black",
+    price_usd: 35,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+
+  {
+    id: "85",
+    name: "Red puffer vest",
+    brand: "Uniqlo U",
+    drawer: "Outerwear",
+    color: "red",
+    price_usd: 70,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "86",
+    name: "White denim vest",
+    brand: "Levi's",
+    drawer: "Outerwear",
+    color: "white",
+    price_usd: 88,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "87",
+    name: "Olive utility vest",
+    brand: "Engineered Garments",
+    drawer: "Outerwear",
+    color: "olive",
+    price_usd: 230,
+    image_url: "/placeholder-img.jpg",
+    favorite: false
+  },
+  {
+    id: "88",
+    name: "Green quilted vest",
+    brand: "Barbour",
+    drawer: "Outerwear",
+    color: "green",
+    price_usd: 165,
+    image_url: "/placeholder-img.jpg",
     favorite: false
   },
   {
